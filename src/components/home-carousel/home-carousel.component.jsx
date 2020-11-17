17df1221dcaf7597ccfc37e11ff6743279683061
KazikKluz <kazik.kluz@gmail.com>
2020-11-17 22:45:01 +0000

    improved and styled carousel

--- src/components/home-carousel/home-carousel.component.jsx
+++ src/components/home-carousel/home-carousel.component.jsx
@@ -2,22 +2,35 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./home-carousel.styles.scss";
 
-const HomeCarousel = () => (
-  <Carousel>
-    <div>
-      <img src="https://picsum.photos/1100/999" alt="" />
-      <p className="legend">Legend 1</p>
-    </div>
-    <div>
-      <img src="https://picsum.photos/1100/999" alt="" />
-      <p className="legend">Legend 2</p>
-    </div>
-    <div>
-      <img src="https://picsum.photos/1100/999" alt="" />
-      <p className="legend">Legend 3</p>
-    </div>
-  </Carousel>
-);
+const HomeCarousel = () => {
+  return (
+    <Carousel
+      infiniteLoop
+      autoPlay
+      showThumbs={false}
+      showStatus={false}
+      showArrows={false}
+      showIndicators={false}
+      stopOnHover={false}
+    >
+      <div className="image-container">
+        <img
+          id="test"
+          width="1200"
+          src="https://picsum.photos/1800/1201"
+          alt=""
+        />
+      </div>
+      <div className="image-container">
+        <img id="test" src="https://picsum.photos/1800/1200" alt="" />
+      </div>
+      <div className="image-container">
+        <img id="test" src="https://picsum.photos/1800/1199" alt="" />
+      </div>
+    </Carousel>
+  );
+};
 
 export default HomeCarousel;
