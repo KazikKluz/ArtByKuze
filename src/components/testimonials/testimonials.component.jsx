895e4976d4af064ca00e2e117a6c12158f046a36
KazikKluz <kazik.kluz@gmail.com>
2021-01-04 17:02:27 +0000

    trying to apply the parallax

--- src/components/testimonials/testimonials.component.jsx
+++ src/components/testimonials/testimonials.component.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import * as React from "react";
+import { useEffect, useState } from "react";
 
-import { Box } from "./testimonials.styles";
+import "./testimonials.scss";
 
-const Testimonials = () => {
-  return <Box></Box>;
-};
+import { Hero } from "./testimonials.styles.js";
 
-export default Testimonials;
+export default function Testimonials() {
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div>
+      <Hero>
+        <img
+          src="https://picsum.photos/1600/1900"
+          alt="test"
+          className="parallax"
+          style={{
+            transform: `translateY(${offset * 0.5}px)`,
+          }}
+        />
+        <div className="text-wrapper">
+          <h1 className="headline">Parallax</h1>
+          <h2 className="sub-headline">Scrolling effect</h2>
+        </div>
+      </Hero>
+    </div>
+  );
+}
